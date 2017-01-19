93ee77ceca54a419d8aa1349fe9d394f4b940d86
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2017-01-19 16:50:33 +0200

    logging some stufff.

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const sendImageMessage = (recipientId) => {
     let renderExport = renderer.renderFrame({
         x: 0, y: -120, z: 20 + Math.random() * 40
     });
-    renderExport.pngStream().pipe(res);
+    
     console.log(`
         --------------------------------------------
             ${renderExport.pack()}
