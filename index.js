cb5802f62046e0e0009e797ff55695d01eea6841
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2017-01-20 02:03:09 +0200

    typo

--- index.js
+++ index.js
@@ -73,7 +73,6 @@ const sendImageMessage = (recipientId) => {
             recipient: {
                 id: recipientId
             },
-            sender_action: "typing_on",
             message: {
                attachment: {
                    type: 'image',
